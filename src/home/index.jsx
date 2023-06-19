4338e2be05d7c5033259a6fb959412fdde66f0e1
alice-sol <127445097+alice-sol@users.noreply.github.com>
2023-06-18 22:05:09 -0300

    Cria tela home

---
+++ src/home/index.jsx
@@ -0,0 +1,134 @@
+import Grid from "@mui/material/Grid";
+import React, { useEffect, useState, useCallback } from "react";
+import Header from "./header";
+import Search from "./search";
+import ProductList from "./product-list";
+import ProductService from "./service";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Alert } from "@mui/material";
+
+const Home = () => {
+  const [productList, setProductList] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const service = new ProductService();
+
+  const getAllProducts = useCallback(async () => {
+    setLoading(true);
+    try {
+      const list = await service.getAllProducts();
+      setProductList(list);
+      setError(false);
+    } catch(err) {
+      setError(true);
+      console.error("Erro ao listar produtos", err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const getAllCategories = useCallback(async () => {
+    setLoading(true);
+    try {
+      const list = await service.getAllCategories();
+      setCategories(list);
+      setError(false);
+    } catch(err) {
+      console.error("Erro ao listar categorias", err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const onSearch = async (searchValue) => {
+    if(!searchValue){
+      getAllProducts();
+    } else {
+    setLoading(true);
+    try {
+      const searchResponse = await service.searchProduct(searchValue);
+      setProductList(searchResponse);
+      setError(false);
+    } catch (err) {
+      console.err("Erro ao buscar produto", err);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+  };
+
+  const onChangeCategory = async (category) => {
+    if(category==='Todos'){
+        getAllProducts();
+    } else {
+      setLoading(true);
+      try {
+        const categoryProductList = await service.changeCategory(category);
+        setProductList(categoryProductList);
+        setError(false);
+      } catch (err) {
+        console.error("Erro ao buscar produtos da categoria", err);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getAllProducts();
+    getAllCategories();
+  }, [getAllProducts, getAllCategories]);
+
+  return (
+ 
+    <Grid
+      container
+      columnSpacing={3}
+      alignItems="flex-start"
+      direction="column"
+    >
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+
+      <Grid item xs={12}>
+      <div 
+      className="container"
+      >
+        <Search
+          onSearch={onSearch}
+          onChangeCategory={onChangeCategory}
+          categories={categories}
+        />
+        </div>
+      </Grid>
+      <Grid item>
+        <div className="list">
+          <Grid
+            container
+            padding={1}
+            rowSpacing={2}
+            columnSpacing={2}
+            justifyContent="flex-start"
+           alignItems="flex-start"
+            wrap="wrap"
+          >
+            {error ? (
+              <Alert severity='error'>Ocorreu um erro, tente novamente mais tarde</Alert>
+            ) : loading ? (
+              <div className="circular-container">
+                <CircularProgress color="inherit" />
+              </div>
+            ) : (
+              <ProductList productList={productList} />
+            )}
+          </Grid>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
+export default Home;
